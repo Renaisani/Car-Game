ee1929b5bb463b8f279449d63e2afd70a54ed614
Imani Mwamlima <imwamlima@gmail.com>
2022-05-25 23:43:02 -0400

    Clean up code

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-i = 1
 car_state = False
-while i == 1:
-    user_input = input(">")
-    if user_input.lower() == "help":
+while True:
+    user_input = input(">").lower()
+    if user_input == "help":
         print("start - start your car")
         print("stop - stop your car")
         print("quit - quit the game")
         print("")
-    elif user_input.lower() == "start":
+    elif user_input == "start":
         if car_state == False:
             print("Car started..Let's Go!")
             car_state = True
         else:
             print("Car is already on")
-    elif user_input.lower() == "stop":
+    elif user_input == "stop":
         if car_state == True:
             print("Car has stopped")
             car_state = False
         else:
             print("Car is already off")
-    elif user_input.lower() == "quit":
+    elif user_input == "quit":
         break
     else:
         print("I don't understand")
